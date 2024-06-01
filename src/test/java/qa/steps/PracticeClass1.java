4e336df79fe409aa8457f9462906260a1d61581c
Luis <Luis@DESKTOP-TSUKEHB>
2024-05-31 19:14:03 -0700

    5/31

--- src/test/java/qa/steps/PracticeClass1.java
+++ src/test/java/qa/steps/PracticeClass1.java
@@ -6,27 +6,20 @@ import qa.pages.Blazedemo;
 
 public class PracticeClass1 {
 	Blazedemo bd;
-	
-	
+
 	@When(".* enterValue \"(.*)\"\"(.*)\"$")
 	public void user_enters_name_enter_value(String string) throws InterruptedException {
-		
-
-		
 	}
 
 	@When(".* selectsValue \"(.*)\"\"(.*)\"$")
 	public void user_selects_card_type_selects_value(String string) throws InterruptedException {
-		
 	}
 
 	@When(".* click \"(.*)\"$")
 	public void user_clicks_remember_me_box_click(String string) throws InterruptedException {
-		
 	}
 
 	@Then(".* verifiesValue \"(.*)\"\"(.*)\"$")
 	public void user_verifies_confirmation_message_verifies_value(String string) throws InterruptedException {
-	
 	}
 }
